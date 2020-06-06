c0648e5236b4834ce84e6edff08cbde43bcb8b68
tanaychaulinsec <tanaychauli@gmail.com>
2020-06-06 16:25:53 +0530

    June Leetcoding challenge Day 6 Optimization

--- ReconstructQueue.py
+++ ReconstructQueue.py
@@ -15,3 +15,12 @@ class Solution:
             else:
                 res+=group
         return res
+
+#Optimization:-
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
